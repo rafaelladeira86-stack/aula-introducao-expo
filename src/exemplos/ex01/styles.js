4f8fa60c95079e5e18b0c74848ed8d0c5aacb20f
rafaelladeira86-stack <rafaelladeira86@gmail.com>
2025-08-22 21:16:55 -0300

    aula22-08

---
+++ src/exemplos/ex01/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create(
+    {
+        container:{
+            backgroundColor:'#ff0',
+            flex:1,
+            alignItems:'center',
+            justifyContent:'center',
+            
+        },
+
+        titulo:{
+            fontSize:25,
+            fontWeight:'black',
+            color:'#777',
+
+
+        },
+
+    }
+);
+
+export default styles;
